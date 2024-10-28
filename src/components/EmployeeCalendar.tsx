ebfada39c9b5aa8c5b3d517378afae2f2e8da684
Vincent Minicozzi <vinicozzimincent@gmail.com>
2024-10-28 15:10:56 -0400

    add adventure class

--- src/components/EmployeeCalendar.tsx
+++ src/components/EmployeeCalendar.tsx
@@ -83,6 +83,8 @@ export default function EmployeeCalendar({ name }: EmployeeCalendarProps) {
       return "bg-red-100 border-red-400 text-red-800";
     } else if (title.includes("Holiday Class")) {
       return "bg-blue-100 border-blue-400 text-blue-800";
+    } else if (title.includes("Adventure Class")) {
+      return "bg-purple-100 border-purple-400 text-purple-800";
     } else if (title.includes("Class")) {
       return "bg-green-100 border-green-400 text-green-800";
     } else if (title.includes("Camp")) {
